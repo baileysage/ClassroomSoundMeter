724f9761686ba8d548a25b00eaf443acd3ce0164
Bailey Steinfadt <baileysteinfadt@gmail.com>
2019-10-14 21:24:06 -0500

    Added output commands

---
+++ ClassroomSoundMeter/src/SoundIO.cpp
@@ -0,0 +1,155 @@
+#include "SoundIO.h"
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <Arduino.h>
+
+/*
+ Heavily borrowing from Adafruit's code at https://learn.adafruit.com/led-ampli-tie/the-code
+*/
+
+SoundIO::SoundIO(){
+    analogReference(EXTERNAL); //tie AREF pin and mic power to 3.3V
+    peak = 16;
+    dotCount = 0;
+    dotHangCount = 0;
+    sample = 0;
+    pinMode(WARN_OUT_PIN, OUTPUT);
+    digitalWrite(WARN_OUT_PIN, HIGH);
+    pinMode(ALARM_OUT_PIN, OUTPUT);
+    digitalWrite(ALARM_OUT_PIN, HIGH);
+    pinMode(SOUND_ACTIVE_PIN, INPUT_PULLUP);
+}
+
+SoundIO::~SoundIO(){
+}
+
+int SoundIO::checkMic(){
+  unsigned long startMillis= millis();  // Start of sample window
+  float peakToPeak = 0;   // peak-to-peak level
+
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1023;
+  int c;
+
+
+  // collect data for length of sample window (in mS)
+  while (millis() - startMillis < SAMPLE_WINDOW)
+  {
+    sample = analogRead(MIC_PIN);
+    if (sample < 1024)  // toss out spurious readings
+    {
+      if (sample > signalMax)
+      {
+        signalMax = sample;  // save just the max levels
+      }
+      else if (sample < signalMin)
+      {
+        signalMin = sample;  // save just the min levels
+      }
+    }
+  }
+  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+ 
+  // Serial.println(peakToPeak);
+
+
+  //Scale the input logarithmically instead of linearly
+  c = fscale(INPUT_FLOOR, INPUT_CEILING, METER_CEILING, 0, peakToPeak, 2);
+
+  if(c < peak) {
+    peak = c;        // Keep dot on top
+    dotHangCount = 0;    // make the dot hang before falling
+  }
+
+
+  // Frame based peak dot animation
+  if(dotHangCount > PEAK_HANG) { //Peak pause length
+    if(++dotCount >= PEAK_FALL) { //Fall rate 
+      peak++;
+      dotCount = 0;
+    }
+  } 
+  else {
+    dotHangCount++; 
+  }
+  return 1;
+}
+
+int SoundIO::playWarning(){
+  digitalWrite(WARN_OUT_PIN, LOW);
+  delay(50);
+  digitalWrite(WARN_OUT_PIN, HIGH);
+}
+
+int SoundIO::playAlarm(){
+  digitalWrite(ALARM_OUT_PIN, LOW);
+  delay(50);
+  digitalWrite(ALARM_OUT_PIN, HIGH);
+}
+
+float SoundIO::fscale( float originalMin, float originalMax, float newBegin, float
+newEnd, float inputValue, float curve){
+
+  float OriginalRange = 0;
+  float NewRange = 0;
+  float zeroRefCurVal = 0;
+  float normalizedCurVal = 0;
+  float rangedValue = 0;
+  bool invFlag = 0;
+
+
+  // condition curve parameter
+  // limit range
+
+  if (curve > 10) curve = 10;
+  if (curve < -10) curve = -10;
+
+  curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output 
+  curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
+
+  
+   // Serial.println(curve * 100, DEC);   // multply by 100 to preserve resolution  
+   // Serial.println(); 
+   
+
+  // Check for out of range inputValues
+  if (inputValue < originalMin) {
+    inputValue = originalMin;
+  }
+  if (inputValue > originalMax) {
+    inputValue = originalMax;
+  }
+
+  // Zero Refference the values
+  OriginalRange = originalMax - originalMin;
+
+  if (newEnd > newBegin){ 
+    NewRange = newEnd - newBegin;
+  }
+  else
+  {
+    NewRange = newBegin - newEnd; 
+    invFlag = 1;
+  }
+
+  zeroRefCurVal = inputValue - originalMin;
+  normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
+
+  // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine 
+  if (originalMin > originalMax ) {
+    return 0;
+  }
+
+  if (invFlag == 0){
+    rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
+
+  }
+  else     // invert the ranges
+  {   
+    rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange); 
+  }
+
+  return rangedValue;
+}
+
---
+++ ClassroomSoundMeter/src/SoundIO.h
@@ -0,0 +1,29 @@
+// Controls MAX4466 breakout from Adafruit
+#define MIC_PIN        A0  // SoundIO is attached to this analog pin
+#define WARN_OUT_PIN   10  
+#define ALARM_OUT_PIN  11
+#define SOUND_ACTIVE_PIN 12
+#define SAMPLE_WINDOW   10  // Sample window for average level
+#define PEAK_HANG 24 //Time of pause before peak dot falls
+#define PEAK_FALL 4 //Rate of falling peak dot
+#define INPUT_FLOOR 10 //Lower range of analogRead input
+#define INPUT_CEILING 300 //Max range of analogRead input, the lower the value the more sensitive (1023 = max)
+#define METER_CEILING 3
+
+class SoundIO{
+    public:
+        SoundIO();
+	    virtual ~SoundIO();
+        int checkMic();
+        int playWarning();
+        int playAlarm();
+    private: 
+        float fscale( float originalMin, float originalMax, float newBegin, float
+        newEnd, float inputValue, float curve);
+
+        short peak;      // Peak level of column; used for falling dots
+        unsigned int sample;
+
+        short dotCount;  //Frame counter for peak dot
+        short dotHangCount; //Frame counter for holding peak dot
+};
